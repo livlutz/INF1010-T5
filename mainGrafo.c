830a19cbc335fbb439571b10529891961c43ba91
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-06-27 23:50:51 -0300

    Update mainGrafo.c

--- mainGrafo.c
+++ mainGrafo.c
@@ -4,7 +4,7 @@
 
 struct viz {
     char noj;
-    float peso;
+    int peso;
     Viz* prox;
 };
 
@@ -15,23 +15,25 @@ struct grafo {
 };
 
 
-int main(void){
-    
-    int numVertices;
+int main(void) {
 
     /*Cria um grafo de 7 vértices e 13 arestas */
     Grafo* g = grafoCria(7, 13);
 
-    numVertices = g->nv;
+    /*Guarda o número de vértices do grafo*/
+    int numVertices = g->nv;
 
-    /*Cria as arestas do grafo */
+    /*Array com as letras que estarão contidas em cada vértice */
 
-    char letras[7] = {'A', 'B', 'C', 'D', 'E', 'F', 'H'};
+    char letras[7] = { 'A', 'B', 'C', 'D', 'E', 'F', 'H' };
+
+    /*OBS : Decidimos colocar A como nó 0 pois ele será a origem 
+    para fazer o algoritmo de Dijkstra e o dfs*/
 
     /* Criando vizinhos de A*/
-    g->viz[0] = criaViz(g->viz[0], letras[1], 5.0);
-    g->viz[0] = criaViz(g->viz[0], letras[2], 4.0);
     g->viz[0] = criaViz(g->viz[0], letras[3], 2.0);
+    g->viz[0] = criaViz(g->viz[0], letras[2], 4.0);
+    g->viz[0] = criaViz(g->viz[0], letras[1], 5.0);
 
     /* Criando vizinhos de B*/
     g->viz[1] = criaViz(g->viz[1], letras[0], 5.0);
@@ -40,10 +42,10 @@ int main(void){
     g->viz[1] = criaViz(g->viz[1], letras[6], 9.0);
 
     /* Criando vizinhos de C */
-    g->viz[2] = criaViz(g->viz[2], letras[0], 4.0);
-    g->viz[2] = criaViz(g->viz[2], letras[1], 6.0);
     g->viz[2] = criaViz(g->viz[2], letras[3], 3.0);
+    g->viz[2] = criaViz(g->viz[2], letras[0], 4.0);
     g->viz[2] = criaViz(g->viz[2], letras[4], 4.0);
+    g->viz[2] = criaViz(g->viz[2], letras[1], 6.0);
 
     /* Criando vizinhos de D */
     g->viz[3] = criaViz(g->viz[3], letras[0], 2.0);
@@ -52,23 +54,25 @@ int main(void){
     g->viz[3] = criaViz(g->viz[3], letras[5], 9.0);
 
     /* Criando vizinhos de E */
-    g->viz[4] = criaViz(g->viz[4], letras[1], 6.0);
+    g->viz[4] = criaViz(g->viz[4], letras[5], 2.0);
     g->viz[4] = criaViz(g->viz[4], letras[2], 4.0);
     g->viz[4] = criaViz(g->viz[4], letras[3], 5.0);
-    g->viz[4] = criaViz(g->viz[4], letras[5], 2.0);
+    g->viz[4] = criaViz(g->viz[4], letras[1], 6.0);
     g->viz[4] = criaViz(g->viz[4], letras[6], 6.0);
 
     /* Criando vizinhos de F */
-    g->viz[5] = criaViz(g->viz[5], letras[3], 9.0);
     g->viz[5] = criaViz(g->viz[5], letras[4], 2.0);
     g->viz[5] = criaViz(g->viz[5], letras[6], 3.0);
+    g->viz[5] = criaViz(g->viz[5], letras[3], 9.0);
 
     /* Criando vizinhos de H */
-    g->viz[6] = criaViz(g->viz[6], letras[1], 9.0);
-    g->viz[6] = criaViz(g->viz[6], letras[4], 6.0);
     g->viz[6] = criaViz(g->viz[6], letras[5], 3.0);
+    g->viz[6] = criaViz(g->viz[6], letras[4], 6.0);
+    g->viz[6] = criaViz(g->viz[6], letras[1], 9.0);
 
     imprimeGrafo(g);
+
+    dfs(g,letras[0]);
 
 
     return 0;
