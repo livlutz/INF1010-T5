a157d763fe674f2b431df05d78577ff1ae28cf02
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-06-27 23:49:51 -0300

    definindo funcs novas

--- grafo.h
+++ grafo.h
@@ -1,5 +1,7 @@
 typedef struct grafo Grafo;
 typedef struct viz Viz;
 
+Viz* criaViz(Viz* head, char noj, int peso);
 Grafo* grafoCria(int nv, int na);
-void imprimeGrafo(Grafo *g);
+void imprimeGrafo(Grafo* g);
+void dfs(Grafo*g,char v);
