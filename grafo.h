6eaaa784178da117444ae8e1991bbe36178997f5
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2023-06-28 17:06:57 -0300

    Definições para a dfs

--- grafo.h
+++ grafo.h
@@ -4,9 +4,13 @@ Luiza Marcondes Paes Leme - 2210275
 
 typedef struct grafo Grafo;
 typedef struct viz Viz;
-typedef struct pilha Pilha;
+
+int retornaIndice(char v);
 
 Viz* criaViz(Viz* head, char noj, int peso);
+
 Grafo* grafoCria(int nv, int na);
+
 void imprimeGrafo(Grafo* g);
-void dfs(Grafo*g,char v, Pilha pilha);
+
+void dfs(Grafo* g, char v);
